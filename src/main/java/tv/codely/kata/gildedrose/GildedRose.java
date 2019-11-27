686134d13655b886c041ecafbdfb7fa9eb8d8480
isabelkm77 <isabel.saa@km77.com>
2019-11-27 16:06:42 +0100

    [REFACTOR] - Refactor some hardcore values into static context

--- src/main/java/tv/codely/kata/gildedrose/GildedRose.java
+++ src/main/java/tv/codely/kata/gildedrose/GildedRose.java
@@ -3,16 +3,20 @@ package tv.codely.kata.gildedrose;
 class GildedRose {
     Item[] items;
 
+    private static final String NAME = "Aged Brie";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String GROUP = "Sulfuras, Hand of Ragnaros";
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals(this.NAME)
+                    && !items[i].name.equals(this.BACKSTAGE_PASSES)) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].name.equals(this.GROUP)) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -20,7 +24,7 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].name.equals(this.BACKSTAGE_PASSES)) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -36,15 +40,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(this.GROUP)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!items[i].name.equals(this.NAME)) {
+                    if (!items[i].name.equals(this.BACKSTAGE_PASSES)) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!items[i].name.equals(this.GROUP)) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
