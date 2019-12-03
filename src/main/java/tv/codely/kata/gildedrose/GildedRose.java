345da026a3491836d9551e88abbd9b1c942c9fc9
isabelkm77 <isabel.saa@km77.com>
2019-12-03 16:12:38 +0100

    [REFACTOR] - Removed unused code

--- src/main/java/tv/codely/kata/gildedrose/GildedRose.java
+++ src/main/java/tv/codely/kata/gildedrose/GildedRose.java
@@ -3,64 +3,57 @@ package tv.codely.kata.gildedrose;
 class GildedRose {
     Item[] items;
 
-    private static final String NAME = "Aged Brie";
+    private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String GROUP = "Sulfuras, Hand of Ragnaros";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(this.NAME)
-                    && !items[i].name.equals(this.BACKSTAGE_PASSES)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(this.GROUP)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item:items) {
+            if (item.name.equals(this.AGED_BRIE)
+            || item.name.equals(this.BACKSTAGE_PASSES)) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals(this.BACKSTAGE_PASSES)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                    if (item.name.equals(this.BACKSTAGE_PASSES)) {
+                        if (item.sellIn < 11 && item.quality < 50) {
+                                item.quality = item.quality + 1;
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        if (item.sellIn < 6 && item.quality < 50) {
+                                item.quality = item.quality + 1;
                         }
                     }
                 }
+            }else{
+                if (item.quality > 0 && !item.name.equals(this.SULFURAS)) {
+                    item.quality = item.quality - 1;
+                }
             }
 
-            if (!items[i].name.equals(this.GROUP)) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.name.equals(this.SULFURAS)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(this.NAME)) {
-                    if (!items[i].name.equals(this.BACKSTAGE_PASSES)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(this.GROUP)) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                 if(item.name.equals(this.AGED_BRIE) &&(item.quality < 50)) {
+                    item.quality = item.quality + 1;
+                }else{
+                     if(item.name.equals(this.BACKSTAGE_PASSES)) {
+                        item.quality = item.quality - item.quality;
+                    }else {
+                        if (item.quality > 0) {
+                            if (!item.name.equals(this.SULFURAS)) {
+                                item.quality = item.quality - 1;
                             }
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
                     }
                 }
             }
         }
-    }
+
+    }   
 }
